07805e5ffe586be1ef1473a9f678acfd60274612
xneyk <aguadenadaledgar@gmail.com>
2023-02-01 22:10:01 +0000

    fix(Header): fix cursor pointer, logo to btn

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 export default function Header(){
    return(
       <div className={header.container}>
-         <div className={header.logo}>
+         <button className={header.logo}>
             <h1 className={header.text}>NowListening</h1>
             <Image 
                alt='Now Listening logo'
                src={'/logo.png'}
                width="42"
                height="34"/>
-         </div>
+         </button>
       </div>
    )
 }
