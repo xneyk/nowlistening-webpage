6fb2e4faef6b7e740b53df02e8d2dee4d7b7897e
xneyk <aguadenadaledgar@gmail.com>
2025-11-27 01:01:01 +0100

    fix: rebuild project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
-}
+  output: 'export',
+  basePath: '/nowlistening-webpage',        // nom del repo de github per a GITHUB PAGES
+  assetPrefix: '/nowlistening-webpage/'    // prefix per CSS/JS/assets
+};
 
 module.exports = nextConfig
