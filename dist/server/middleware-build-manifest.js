3887f828582d0eea8594777ce72fa72b60b46224
xneyk <aguadenadaledgar@gmail.com>
2025-11-27 01:22:42 +0100

    fix

---
+++ dist/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/4ddd5374f061932f.js",
+    "static/chunks/77e9c345519e29f8.js",
+    "static/chunks/0cf7232c6ff29088.js",
+    "static/chunks/turbopack-15e08fd838f63a6e.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
