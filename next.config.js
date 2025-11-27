e9959fc8e1899ecee61d0b6a90f0cb2612323902
xneyk <aguadenadaledgar@gmail.com>
2025-11-27 01:34:17 +0100

    feat: production images fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV;
+
 const nextConfig = {
+  basePath: isProduction ? 'nowlistening-webpage' : '',
   out: 'export',
   distDir: 'dist'
 };
