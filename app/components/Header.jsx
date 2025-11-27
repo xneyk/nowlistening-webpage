7a0cbb2baeb743511c3ef706d245b20526e530bc
xneyk <aguadenadaledgar@gmail.com>
2025-11-27 01:47:58 +0100

    feat: fixing url images

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,13 +2,15 @@ import header from './header.module.css'
 import Image from 'next/image'
 
 export default function Header(){
+   const basePath = 'nowlistening-webpage/'; // used for GitHub Pages Production Version
+
    return(
       <div className={header.container}>
          <button className={header.logo}>
             <h1 className={header.text}>NowListening</h1>
             <Image 
                alt='Now Listening logo'
-               src={'/logo.png'}
+               src={`/${basePath}logo.png`}
                width="42"
                height="34"/>
          </button>
