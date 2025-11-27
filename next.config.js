1851a5b4e9b8e18d8ab27a83e0bdc2a58cc371ae
xneyk <aguadenadaledgar@gmail.com>
2025-11-27 01:17:12 +0100

    fix: production attempt

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/nowlistening-webpage',        // nom del repo de github per a GITHUB PAGES
-  assetPrefix: '/nowlistening-webpage/'    // prefix per CSS/JS/assets
+  out: 'export',
+  distDir: 'dist'
 };
 
 module.exports = nextConfig
